11d43968753ea6effe8aead5f68f189bb920480c
Sithembiso Chivambo <schivambo@uj.ac.za>
2025-05-08 22:06:25 +0200

    Fix recursive build script

---
+++ frontend/__mocks__/react-router-dom.js
@@ -0,0 +1,5 @@
+export const BrowserRouter = ({ children }) => <div>{children}</div>;
+export const Routes = ({ children }) => <div>{children}</div>;
+export const Route = () => null;
+export const useNavigate = () => jest.fn();
+export const useParams = () => ({});
